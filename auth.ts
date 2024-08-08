01b417ac5a5439295ee714110e2ec107f3e16df7
Girgetto <girgetto@users.noreply.github.com>
2024-08-08 17:30:06 +0200

    chore: Update NextAuth configuration and add Ping provider

--- auth.ts
+++ auth.ts
@@ -1,39 +1,10 @@
-import NextAuth from "next-auth"
-import "next-auth/jwt"
-
-import Apple from "next-auth/providers/apple"
-import Auth0 from "next-auth/providers/auth0"
-import AzureB2C from "next-auth/providers/azure-ad-b2c"
-import BankIDNorway from "next-auth/providers/bankid-no"
-import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
-import Cognito from "next-auth/providers/cognito"
-import Coinbase from "next-auth/providers/coinbase"
-import Discord from "next-auth/providers/discord"
-import Dropbox from "next-auth/providers/dropbox"
-import Facebook from "next-auth/providers/facebook"
-import GitHub from "next-auth/providers/github"
-import GitLab from "next-auth/providers/gitlab"
-import Google from "next-auth/providers/google"
-import Hubspot from "next-auth/providers/hubspot"
-import Keycloak from "next-auth/providers/keycloak"
-import LinkedIn from "next-auth/providers/linkedin"
-import Netlify from "next-auth/providers/netlify"
-import Okta from "next-auth/providers/okta"
-import Passage from "next-auth/providers/passage"
-import Passkey from "next-auth/providers/passkey"
-import Pinterest from "next-auth/providers/pinterest"
-import Reddit from "next-auth/providers/reddit"
-import Slack from "next-auth/providers/slack"
-import Spotify from "next-auth/providers/spotify"
-import Twitch from "next-auth/providers/twitch"
-import Twitter from "next-auth/providers/twitter"
-import WorkOS from "next-auth/providers/workos"
-import Zoom from "next-auth/providers/zoom"
-import { createStorage } from "unstorage"
-import memoryDriver from "unstorage/drivers/memory"
-import vercelKVDriver from "unstorage/drivers/vercel-kv"
-import { UnstorageAdapter } from "@auth/unstorage-adapter"
-import type { NextAuthConfig } from "next-auth"
+import NextAuth from "next-auth";
+import "next-auth/jwt";
+import { createStorage } from "unstorage";
+import memoryDriver from "unstorage/drivers/memory";
+import vercelKVDriver from "unstorage/drivers/vercel-kv";
+import { UnstorageAdapter } from "@auth/unstorage-adapter";
+import type { NextAuthConfig } from "next-auth";
 
 const storage = createStorage({
   driver: process.env.VERCEL
@@ -43,96 +14,75 @@ const storage = createStorage({
         env: false,
       })
     : memoryDriver(),
-})
+});
 
 const config = {
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   adapter: UnstorageAdapter(storage),
   providers: [
-    Apple,
-    Auth0,
-    AzureB2C({
-      clientId: process.env.AUTH_AZURE_AD_B2C_ID,
-      clientSecret: process.env.AUTH_AZURE_AD_B2C_SECRET,
-      issuer: process.env.AUTH_AZURE_AD_B2C_ISSUER,
-    }),
-    BankIDNorway,
-    BoxyHQSAML({
-      clientId: "dummy",
-      clientSecret: "dummy",
-      issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
-    }),
-    Cognito,
-    Coinbase,
-    Discord,
-    Dropbox,
-    Facebook,
-    GitHub,
-    GitLab,
-    Google,
-    Hubspot,
-    Keycloak({ name: "Keycloak (bob/bob)" }),
-    LinkedIn,
-    Netlify,
-    Okta,
-    Passkey({
-      formFields: {
-        email: {
-          label: "Username",
-          required: true,
-          autocomplete: "username webauthn",
+    {
+      authorization: {
+        url: `https://auth.pingone.eu/${process.env.PING_TENANT_ID}/as/authorize`,
+        params: {
+          response_type: "code",
+          scope: "openid profile email",
         },
       },
-    }),
-    Passage,
-    Pinterest,
-    Reddit,
-    Slack,
-    Spotify,
-    Twitch,
-    Twitter,
-    WorkOS({
-      connection: process.env.AUTH_WORKOS_CONNECTION!,
-    }),
-    Zoom,
+      type: "oauth" as const,
+      id: "ping",
+      name: "Ping",
+      wellKnown: `https://auth.pingone.eu/${process.env.PING_TENANT_ID}/as/.well-known/openid-configuration`,
+      userinfo: `https://auth.pingone.eu/${process.env.PING_TENANT_ID}/as/userinfo`,
+      token: `https://auth.pingone.eu/${process.env.PING_TENANT_ID}/as/token`,
+      profile: (profile: any) => {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+      clientId: process.env.PING_CLIENT_ID,
+      clientSecret: process.env.PING_CLIENT_SECRET,
+      accessTokenUrl: `https://auth.pingone.eu/${process.env.PING_TENANT_ID}/as/token`,
+    },
   ],
   basePath: "/auth",
   callbacks: {
-    authorized({ request, auth }) {
-      const { pathname } = request.nextUrl
-      if (pathname === "/middleware-example") return !!auth
-      return true
-    },
-    jwt({ token, trigger, session, account }) {
-      if (trigger === "update") token.name = session.user.name
-      if (account?.provider === "keycloak") {
-        return { ...token, accessToken: account.access_token }
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = account.providerAccountId;
       }
-      return token
+      return token;
     },
-    async session({ session, token }) {
-      if (token?.accessToken) {
-        session.accessToken = token.accessToken
-      }
-      return session
+    async session({ session, token }: { session: any; token: JWT }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+
+      return session;
     },
   },
   experimental: {
     enableWebAuthn: true,
   },
   debug: process.env.NODE_ENV !== "production" ? true : false,
-} satisfies NextAuthConfig
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+} satisfies NextAuthConfig;
 
-export const { handlers, auth, signIn, signOut } = NextAuth(config)
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
 
 declare module "next-auth" {
   interface Session {
-    accessToken?: string
+    accessToken?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    accessToken?: string
+    accessToken?: string;
   }
 }
